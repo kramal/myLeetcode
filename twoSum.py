1547da4d8b2d23ac25d1f4d9bf0b54e572f812fc
Almas Mirzakhmetov <kramal888@gmail.com>
2020-11-15 22:27:27 +0600

    Update twoSum.py
    comments time/space complexity

--- twoSum.py
+++ twoSum.py
@@ -1,7 +1,10 @@
+# Time complexity = O(n)
+# Space complexity = O(n)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         i = 0
         j = len(nums)-1
+        pairs = []
         
         old_dict = {}
         for k in range(0,j+1):
@@ -9,15 +12,14 @@ class Solution:
      
         new_dict = {k: v for k, v in sorted(old_dict.items(), key=lambda item: item[1])}
         
-        pairs = []
-        
         new_nums = list(new_dict.values())
+        indexes = list(new_dict.keys())
         
         while i < j:
             current_sum = new_nums[i] + new_nums[j]
             
             if current_sum == target:
-                pairs.append([list(new_dict.keys())[i], list(new_dict.keys())[j]])
+                pairs.append([ indexes[i], indexes[j] ])
             
             if current_sum < target:
                 i = i + 1
