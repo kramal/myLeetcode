94eba81f5a234ef276a67c13ad59744b07772a4b
Almas M <kramal888@gmail.com>
2020-11-29 14:32:08 +0600

    single number

---
+++ singleNumber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        num_count_hash = {}
+
+        for i in range(0, len(nums)):
+            count = num_count_hash.get(nums[i])
+            if count is None:
+                num_count_hash[nums[i]] = 1
+            else:
+                num_count_hash[nums[i]] = num_count_hash[nums[i]] + 1
+
+        key_min = min(num_count_hash.keys(), key=(lambda k: num_count_hash[k]))
+
+        return key_min
